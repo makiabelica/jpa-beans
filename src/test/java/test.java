3d1b26e2ba0b9fd99a3328b7c4cdf43ad979f9db
angie <angie.espinoza.campos@gmail.com>
2024-03-17 17:47:57 -0600

    final

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -12,28 +12,31 @@ public class test {
     public static void main(String[] args) {
         //crearEstudiante();
         //recuperarPorId();
-        //actulizarEstudiante()
+        //actulizarEstudiante();
+        //eliminarEstudiante();
     }
 
-    private static void crearEstudiante(){
+    public static void crearEstudiante() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EstudiantePU");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        Estudiante estudiante = new Estudiante(2, "EC100521", "Angie", "Espinoza");
+
+        Estudiante estudiante = new Estudiante(2,"Angie", "Feliz", "como una lombris");
         em.persist(estudiante);
         tx.commit();
-        log.debug("Objecto" + estudiante);
+        log.debug("Objeto" + estudiante);
         em.close();
     }
+
     private static void recuperarPorId(){
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EstudiantePU");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        Estudiante estudiante = em.find(Estudiante.class, 2);
+        Estudiante estudiante = em.find(Estudiante.class, 1);
         tx.commit();
-        System.out.println("Objeto" +  estudiante);
+        System.out.println("Objeto Recuperado" +  estudiante);
         em.close();
     }
 
@@ -42,17 +45,18 @@ public class test {
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
-        Estudiante estudiante = em.find(Estudiante.class, 2);
+        Estudiante estudiante = em.find(Estudiante.class, 1);
         tx.commit();
-        System.out.println("Objeto Recuperado" +  estudiante);
+        System.out.println("Objeto actualizado" +  estudiante);
 
-        estudiante.setNombre("Angie");
+        estudiante.setNombre("Feli");
+        estudiante.setApellido("Como una lombrish");
         EntityTransaction tx2 = em.getTransaction();
         tx2.begin();
 
         em.merge(estudiante);
         tx2.commit();
-        System.out.println("Objeto" +  estudiante);
+        System.out.println("Objeto Eliminado" +  estudiante);
 
         em.close();
     }
@@ -61,18 +65,30 @@ public class test {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EstudiantePU");
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
-        tx.begin();
+
+        tx.begin(); // INICIAMOS TRANSACCION
         Estudiante estudiante = em.find(Estudiante.class, 2);
         tx.commit();
-        System.out.println("Objeto Recuperado" +  estudiante);
 
-        estudiante.setNombre("Angie");
-        EntityTransaction tx2 = em.getTransaction();
-        tx2.begin();
+        if (estudiante != null) {
+            System.out.println("Estudiante encontrado para eliminar:");
+            System.out.println("ID: " + estudiante.getIdestudiante());
+            System.out.println("Nombre: " + estudiante.getNombre());
+            System.out.println("Apellido: " + estudiante.getApellido());
 
-        em.merge(estudiante);
-        tx2.commit();
-        System.out.println("Objeto" +  estudiante);
+            EntityTransaction tx2 = em.getTransaction();
+            tx2.begin();
+
+            em.remove(em.merge(estudiante));
+            tx2.commit();
+
+            System.out.println("Estudiante eliminado correctamente:");
+            System.out.println("ID: " + estudiante.getIdestudiante());
+            System.out.println("Nombre: " + estudiante.getNombre());
+            System.out.println("Apellido: " + estudiante.getApellido());
+        } else {
+            System.out.println("No se encontró ningún estudiante con ID 1 para eliminar.");
+        }
 
         em.close();
     }
